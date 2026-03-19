0a1150e138febbbc4ac1367995e3a49649577221
Konstantin S. <havendv@gmail.com>
2026-03-19 17:13:23 +0400

    feat: Updated OpenAPI spec (#85)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Weaviate/Generated/Weaviate.JsonConverters.BatchReferenceResponse.g.cs
+++ src/libs/Weaviate/Generated/Weaviate.JsonConverters.BatchReferenceResponse.g.cs
@@ -15,29 +15,91 @@ namespace Weaviate.JsonConverters
             options = options ?? throw new global::System.ArgumentNullException(nameof(options));
             var typeInfoResolver = options.TypeInfoResolver ?? throw new global::System.InvalidOperationException("TypeInfoResolver is not set.");
 
-            var
-            readerCopy = reader;
-            global::Weaviate.BatchReference? value1 = default;
-            try
+            using var __jsonDocument = global::System.Text.Json.JsonDocument.ParseValue(ref reader);
+            var __rawJson = __jsonDocument.RootElement.GetRawText();
+            var __jsonProps = new global::System.Collections.Generic.HashSet<string>();
+            if (__jsonDocument.RootElement.ValueKind == global::System.Text.Json.JsonValueKind.Object)
             {
-                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Weaviate.BatchReference), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Weaviate.BatchReference> ??
-                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Weaviate.BatchReference).Name}");
-                value1 = global::System.Text.Json.JsonSerializer.Deserialize(ref readerCopy, typeInfo);
-            }
-            catch (global::System.Text.Json.JsonException)
-            {
+                foreach (var __jsonProp in __jsonDocument.RootElement.EnumerateObject())
+                {
+                    __jsonProps.Add(__jsonProp.Name);
+                }
             }
 
-            readerCopy = reader;
+            var __score0 = 0;
+            if (__jsonProps.Contains("from")) __score0++;
+            if (__jsonProps.Contains("to")) __score0++;
+            if (__jsonProps.Contains("tenant")) __score0++;
+            var __score1 = 0;
+            if (__jsonProps.Contains("result")) __score1++;
+            var __bestScore = 0;
+            var __bestIndex = -1;
+            if (__score0 > __bestScore) { __bestScore = __score0; __bestIndex = 0; }
+            if (__score1 > __bestScore) { __bestScore = __score1; __bestIndex = 1; }
+
+            global::Weaviate.BatchReference? value1 = default;
             global::Weaviate.BatchReferenceResponseVariant2? value2 = default;
-            try
+            if (__bestIndex >= 0)
             {
-                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Weaviate.BatchReferenceResponseVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Weaviate.BatchReferenceResponseVariant2> ??
-                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Weaviate.BatchReferenceResponseVariant2).Name}");
-                value2 = global::System.Text.Json.JsonSerializer.Deserialize(ref readerCopy, typeInfo);
+                if (__bestIndex == 0)
+                {
+                    try
+                    {
+                        var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Weaviate.BatchReference), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Weaviate.BatchReference> ??
+                                       throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Weaviate.BatchReference).Name}");
+                        value1 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
+                    }
+                    catch (global::System.Text.Json.JsonException)
+                    {
+                    }
+                    catch (global::System.InvalidOperationException)
+                    {
+                    }
+                }
+                else if (__bestIndex == 1)
+                {
+                    try
+                    {
+                        var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Weaviate.BatchReferenceResponseVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Weaviate.BatchReferenceResponseVariant2> ??
+                                       throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Weaviate.BatchReferenceResponseVariant2).Name}");
+                        value2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
+                    }
+                    catch (global::System.Text.Json.JsonException)
+                    {
+                    }
+                    catch (global::System.InvalidOperationException)
+                    {
+                    }
+                }
             }
-            catch (global::System.Text.Json.JsonException)
+
+            if (value1 == null && value2 == null)
             {
+                try
+                {
+                    var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Weaviate.BatchReference), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Weaviate.BatchReference> ??
+                                   throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Weaviate.BatchReference).Name}");
+                    value1 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
+                }
+                catch (global::System.Text.Json.JsonException)
+                {
+                }
+                catch (global::System.InvalidOperationException)
+                {
+                }
+
+                try
+                {
+                    var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Weaviate.BatchReferenceResponseVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Weaviate.BatchReferenceResponseVariant2> ??
+                                   throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Weaviate.BatchReferenceResponseVariant2).Name}");
+                    value2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
+                }
+                catch (global::System.Text.Json.JsonException)
+                {
+                }
+                catch (global::System.InvalidOperationException)
+                {
+                }
             }
 
             var __value = new global::Weaviate.BatchReferenceResponse(
@@ -45,19 +107,6 @@ namespace Weaviate.JsonConverters
 
                 value2
                 );
-
-            if (value1 != null)
-            {
-                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Weaviate.BatchReference), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Weaviate.BatchReference> ??
-                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Weaviate.BatchReference).Name}");
-                _ = global::System.Text.Json.JsonSerializer.Deserialize(ref reader, typeInfo);
-            }
-            else if (value2 != null)
-            {
-                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Weaviate.BatchReferenceResponseVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Weaviate.BatchReferenceResponseVariant2> ??
-                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Weaviate.BatchReferenceResponseVariant2).Name}");
-                _ = global::System.Text.Json.JsonSerializer.Deserialize(ref reader, typeInfo);
-            }
 
             return __value;
         }
