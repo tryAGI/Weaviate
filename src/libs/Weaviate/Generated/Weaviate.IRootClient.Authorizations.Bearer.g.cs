f61a5ae0a62dbe6ec2331c5254a905c4c28cbc67
Konstantin S. <havendv@gmail.com>
2026-09-13 19:51:39 +0400

    feat: Updated OpenAPI spec (#143)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/Weaviate/Generated/Weaviate.IRootClient.Authorizations.Bearer.g.cs
@@ -0,0 +1,16 @@
+
+#nullable enable
+
+namespace Weaviate
+{
+    public partial interface IRootClient
+    {
+        /// <summary>
+        /// Authorize using bearer authentication.
+        /// </summary>
+        /// <param name="apiKey"></param>
+
+        public void AuthorizeUsingBearer(
+            string apiKey);
+    }
+}
